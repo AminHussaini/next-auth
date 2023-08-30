6c1603c1bac98b6b788a81d07daf30f48af97e50
Amin Hussain <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-08-31 02:06:30 +0500

    Pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
 
-// const withPWA = require('next-pwa');
+// const withPWA = require("next-pwa");
 
 // module.exports = withPWA({
 //   pwa: {
-//     dest: 'public', // Output directory for generated service worker
-//     disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
 //   },
 // });
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
+
+module.exports = withPWA({
+  // next.js config
+})
